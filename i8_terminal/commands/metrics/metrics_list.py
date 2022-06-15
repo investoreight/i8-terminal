e2483905155fbf742ec054bf77e7f74657368920
Saddy Hadjigi <saddy.hadjigi@gmail.com>
2022-06-15 09:21:11 +0430

    Resolve comments and implement export option fot the command

--- i8_terminal/commands/metrics/metrics_list.py
+++ i8_terminal/commands/metrics/metrics_list.py
@@ -10,8 +10,12 @@ from rich.table import Table
 from i8_terminal.commands.metrics import metrics
 from i8_terminal.common.cli import pass_command
 from i8_terminal.common.layout import df2Table, format_metrics_df
+from i8_terminal.common.utils import export_data
+from i8_terminal.config import APP_SETTINGS
 from i8_terminal.types.metric_param_type import MetricParamType
 from i8_terminal.types.ticker_param_type import TickerParamType
+
+from i8_terminal.common.metrics import get_current_metrics_df, prepare_current_metrics_formatted_df  # isort:skip
 
 
 def render_metrics_table(tickers: str, metricsList: str) -> Optional[Table]:
@@ -49,12 +53,29 @@ def render_metrics_table(tickers: str, metricsList: str) -> Optional[Table]:
     default="pe_ratio_ttm",
     help="Comma-separated list of daily metrics.",
 )
+@click.option("--export", "export_path", "-e", help="Filename to export the output to.")
 @pass_command
-def list(tickers: str, metrics: str) -> None:
+def list(tickers: str, metrics: str, export_path: Optional[str]) -> None:
     console = Console()
     with console.status("Fetching data...", spinner="material"):
-        table = render_metrics_table(tickers, metrics)
-    if table is None:
+        df = get_current_metrics_df(tickers, metrics)
+    if df is None:
         console.print("No data found for metrics with selected tickers", style="yellow")
     else:
-        console.print(table)
+        for m in [*set(metrics.split(",")) - set(df["metric_name"])]:
+            console.print(f"\nNo data found for metric {m} with selected tickers", style="yellow")
+        if export_path:
+            export_data(
+                prepare_current_metrics_formatted_df(df, "store"),
+                export_path,
+                column_width=18,
+                column_format=APP_SETTINGS["styles"]["xlsx"]["financials"]["column"],
+            )
+        else:
+            columns_justify: Dict[str, Any] = {}
+            for metric_display_name, metric_df in df.groupby("display_name"):
+                columns_justify[metric_display_name] = (
+                    "left" if metric_df["display_format"].values[0] == "str" else "right"
+                )
+            table = df2Table(prepare_current_metrics_formatted_df(df, "console"), columns_justify=columns_justify)
+            console.print(table)
