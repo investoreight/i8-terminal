04a3ba4277f32d6cc76c0ebc36f0cd104910d638
SaddyHadjigi <saddy.hadjigi@gmail.com>
2022-10-25 10:59:30 +0330

    Add new functions in screen common file

--- i8_terminal/commands/screen/screen_gainers.py
+++ i8_terminal/commands/screen/screen_gainers.py
@@ -1,32 +1,13 @@
-from typing import Any, Dict, Optional
+from typing import Optional
 
 import click
-import investor8_sdk
-import pandas as pd
 from rich.console import Console
 
 from i8_terminal.commands.screen import screen
 from i8_terminal.common.cli import pass_command
-from i8_terminal.common.layout import df2Table
-from i8_terminal.common.metrics import (
-    get_current_metrics_df,
-    prepare_current_metrics_formatted_df,
-)
-from i8_terminal.common.utils import export_data, export_to_html
-from i8_terminal.config import APP_SETTINGS
+from i8_terminal.common.screen import get_top_stocks_df, render_top_stocks
 from i8_terminal.types.market_indice_param_type import MarketIndiceParamType
 from i8_terminal.types.metric_view_param_type import MetricViewParamType
-
-
-def prepare_gainers_df(index: str, view_name: Optional[str]) -> Optional[pd.DataFrame]:
-    metrics = APP_SETTINGS["commands"]["screen_gainers"]["metrics"]
-    companies_data = investor8_sdk.ScreenerApi().get_top_stocks("winners", index=index)
-    if companies_data is None:
-        return None
-    companies = [company.ticker for company in companies_data]
-    if view_name:
-        metrics = metrics + "," + APP_SETTINGS["metric_view"][view_name]["metrics"]
-    return get_current_metrics_df(",".join(companies), metrics)
 
 
 @screen.command()
@@ -53,34 +34,10 @@ def gainers(index: str, view_name: Optional[str], export_path: Optional[str]) ->
     """
     console = Console()
     with console.status("Fetching data...", spinner="material"):
-        df = prepare_gainers_df(index, view_name)
+        df = get_top_stocks_df("winners", index, view_name)
     if df is None:
         console.print("No data found for today winners", style="yellow")
         return
-    columns_justify: Dict[str, Any] = {}
-    if export_path:
-        if export_path.split(".")[-1] == "html":
-            for metric_display_name, metric_df in df.groupby("display_name"):
-                columns_justify[metric_display_name] = (
-                    "left" if metric_df["display_format"].values[0] == "str" else "right"
-                )
-            table = df2Table(
-                prepare_current_metrics_formatted_df(df, "console").sort_values("Change", ascending=False),
-                columns_justify=columns_justify,
-            )
-            export_to_html(table, export_path)
-            return
-        export_data(
-            prepare_current_metrics_formatted_df(df, "store").sort_values("Change", ascending=False),
-            export_path,
-            column_width=18,
-            column_format=APP_SETTINGS["styles"]["xlsx"]["financials"]["column"],
-        )
-    else:
-        for metric_display_name, metric_df in df.groupby("display_name"):
-            columns_justify[metric_display_name] = "left" if metric_df["display_format"].values[0] == "str" else "right"
-        table = df2Table(
-            prepare_current_metrics_formatted_df(df, "console").sort_values("Change", ascending=False),
-            columns_justify=columns_justify,
-        )
+    table = render_top_stocks(df, export_path=export_path, ascending=False)
+    if table:
         console.print(table)
