21192453fbef3fc34fa3a18c3443a716a2e43336
Investoreight <info@investoreight.com>
2023-02-19 19:32:45 +0100

    updates columns context class

--- i8_terminal/service_result/columns_context.py
+++ i8_terminal/service_result/columns_context.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from i8_terminal.common.metrics import get_all_metrics_df
+from i8_terminal.i8_exception import I8Exception
 from i8_terminal.service_result.column_info import ColumnInfo
 
 
@@ -10,12 +11,25 @@ class ColumnsContext:
         self._enrich_col_infos()
 
     def _enrich_col_infos(self):
-        # Fetch metadata and enrich col info
-        metrics_df = get_all_metrics_df()
+        all_metrics_dict = self.get_metrics_dict()
+
+        for ci in self._col_infos:
+            if ci.col_type == "metric":
+                if ci.name not in all_metrics_dict:
+                    raise I8Exception(
+                        f"Metric `{ci.name}` is not a known metric! You need to explicitly define the metadata!"
+                    )
+                ci.enrich(all_metrics_dict[ci.name])
         return
+
+    def get_metrics_dict(self):
+        metrics_df = get_all_metrics_df()
+        metrics_dict = {}
+        for _, r in metrics_df.iterrows():
+            metrics_df[r["metric_name"]] = ColumnInfo(
+                r["metric_name"], "metric", r["display_name"], r["data_format"], r["unit"]
+            )
+        return metrics_dict
 
     def get_col_infos(self) -> List[ColumnInfo]:
         return self._col_infos
-
-    def get_col_info(self) -> ColumnInfo:
-        return None
