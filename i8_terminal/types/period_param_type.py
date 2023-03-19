de6441464f1e0179c65f8bfb422dd1bc1a765a1f
SaddyHadjigi <saddy.hadjigi@gmail.com>
2023-03-20 01:42:07 +0330

    Update namings

--- i8_terminal/types/period_param_type.py
+++ i8_terminal/types/period_param_type.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Tuple
 from i8_terminal.common.metrics import get_all_metrics_default_period_types_dict
 from i8_terminal.types.auto_complete_choice import AutoCompleteChoice
 
-PERIODS: Dict[str, List[Tuple[str, str]]] = {
+PERIOD_TYPES: Dict[str, List[Tuple[str, str]]] = {
     "D": [("p", "Default period type (daily)")],
     "R": [("p", "Default period type (realtime)")],
     "FY": [
@@ -35,12 +35,14 @@ PERIODS: Dict[str, List[Tuple[str, str]]] = {
 
 class PeriodParamType(AutoCompleteChoice):
     name = "period"
-    metrics: Dict[str, str] = {}
+    metrics_default_pt: Dict[str, str] = {}
 
     def get_suggestions(self, keyword: str, pre_populate: bool = True, metric: str = None) -> List[Tuple[str, str]]:
         if not self.is_loaded:
-            self.metrics = get_all_metrics_default_period_types_dict()
-        self.set_choices(PERIODS.get(self.metrics[metric if metric else ""], [("p", "Default period type (NA)")]))
+            self.metrics_default_pt = get_all_metrics_default_period_types_dict()
+        self.set_choices(
+            PERIOD_TYPES.get(self.metrics_default_pt[metric if metric else ""], [("p", "Default period type (NA)")])
+        )
 
         if pre_populate and keyword.strip() == "":
             return self._choices[: self.size]
