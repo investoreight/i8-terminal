4200fe614b6c6012368eb84c8c4efb264320b9a5
SaddyHadjigi <saddy.hadjigi@gmail.com>
2023-07-12 09:29:39 +0330

    Fix the completion and auto suggest bug

--- i8_terminal/types/i8_auto_suggest.py
+++ i8_terminal/types/i8_auto_suggest.py
@@ -28,7 +28,16 @@ class I8AutoSuggest(AutoSuggest):
         command = ctx.click_ctx.command
 
         if ctx.last_option:
-            matched_params = [p for p in command.params if isinstance(p, click.Option) and ctx.last_option in p.opts]
+            command_string = document.current_line
+            cursor_position = document.cursor_position
+            options_positions = [
+                (o, cursor_position - command_string.find(o))
+                for c in command.params
+                for o in ctx.used_options
+                if cursor_position - command_string.find(o) > 0
+            ]
+            matched = min(options_positions, key=lambda option_position: option_position[1])
+            matched_params = [p for p in command.params if isinstance(p, click.Option) and matched[0] in p.opts]
             if len(matched_params) > 0:
                 matched_param = matched_params[0]
                 if type(matched_param.type) is click.types.DateTime:
